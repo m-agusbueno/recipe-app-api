21ed20e3639c8e87b40969b70b6dd23dc321dfa5
mabueno <magusbueno@gmail.com>
2024-04-15 02:57:00 -0300

    add wait for db to docker compose, checks

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-52b5fr9uxxw!v^ww%ha)(qf)7h+aw2ef5qw5wpg4h7jh4ly)b$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
